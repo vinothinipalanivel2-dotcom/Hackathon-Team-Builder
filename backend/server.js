b45d0851a7e1c1415e86dadc41ce5b3a727fd8e8
vinothinipalanivel2 <vinothinipalanivel2@gmail.com>
2026-06-25 22:15:30 +0530

    Fix Render deployement port

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,9 @@ app.delete("/teams/:id", (req, res) => {
 });
 
 // Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
